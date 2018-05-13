1c47f6ca85c8450ffc73e226d26d604f7bdfe016
eduardosantosgit <eduardosantos058@gmail.com>
2018-05-13 19:59:09 -0300

    add project reader

---
+++ ReaderDocument/CsvReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ReaderDocument
+{
+    public class CsvReader
+    {
+        public IEnumerable<string> Reader(string fileName)
+        {
+            var listAccessKey = new List<string>();
+            var file = new StreamReader(fileName);
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (!string.IsNullOrWhiteSpace(line) && !string.IsNullOrEmpty(line))
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        listAccessKey.Add(line);
+                    }
+                }
+            }
+            file.Close();
+            return listAccessKey;
+        }
+
+    }
+}
---
+++ ReaderDocument/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ReaderDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fileName = "";
+            var xlsx = new ExcelReader().Reader(fileName);
+            var csv = new CsvReader().Reader(fileName);
+        }
+    }
+}
